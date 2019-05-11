8e6dc3fd736b669d78e5de80de8a7c0f3b9efc85
gabriel <gabrielsf14@gmail.com>
2019-05-10 23:42:25 -0230

    icons

--- app/src/main/java/br/com/zinga/ui/materials/adapters/CoursesListAdapter.kt
+++ app/src/main/java/br/com/zinga/ui/materials/adapters/CoursesListAdapter.kt
@@ -34,6 +34,16 @@ class CoursesListAdapter(var courses: List<Course>) : RecyclerView.Adapter<Cours
         when (course.name) {
             "Física" -> holder.ivCourseImg.setImageResource(R.drawable.fisica)
             "Matemática" -> holder.ivCourseImg.setImageResource(R.drawable.mtm)
+            "Química" -> holder.ivCourseImg.setImageResource(R.drawable.quimica)
+            "Biologia" -> holder.ivCourseImg.setImageResource(R.drawable.bio)
+            "História" -> holder.ivCourseImg.setImageResource(R.drawable.history)
+            "Geografia" -> holder.ivCourseImg.setImageResource(R.drawable.geo)
+            "Filosofia" -> holder.ivCourseImg.setImageResource(R.drawable.filosofia)
+            "Sociologia" -> holder.ivCourseImg.setImageResource(R.drawable.sociology)
+            "Português" -> holder.ivCourseImg.setImageResource(R.drawable.portugues)
+            "Redação" -> holder.ivCourseImg.setImageResource(R.drawable.redacao)
+            "Inglês" -> holder.ivCourseImg.setImageResource(R.drawable.translate)
+            "Espanhol" -> holder.ivCourseImg.setImageResource(R.drawable.translate)
             else -> {}
         }
         holder.ivCourseImg.setColorFilter(ContextCompat.getColor(holder.ivCourseImg.context, R.color.colorAccent))
